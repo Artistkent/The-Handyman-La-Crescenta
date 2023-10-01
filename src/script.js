e574f5e39b83ce8bc15ca18f8280971e0146b6a1
Artistkent <collins.ose5@gmail.com>
2023-10-01 13:38:39 +0100

    First commit

---
+++ src/script.js
@@ -0,0 +1,22 @@
+// Implement Intersection Observer for animations or loading as elements come into view
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.2
+  };
+  
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('animate');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, options);
+  
+  const serviceCards = document.querySelectorAll('.service-card');
+  
+  serviceCards.forEach(card => {
+    observer.observe(card);
+  });
+  
